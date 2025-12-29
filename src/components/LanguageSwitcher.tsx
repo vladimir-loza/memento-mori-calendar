b2a9f0a2d5dc599a252f4cbbc1d76fe4184d5553
vladimirLoza <vloza@getoptimachain.com>
2025-12-28 18:55:17 -0600

    ref(LanguageSwitcher): use toggle instead buttons

--- src/components/LanguageSwitcher.tsx
+++ src/components/LanguageSwitcher.tsx
@@ -1,35 +1,35 @@
-import { useTranslation } from 'react-i18next';
-import { ButtonLangSwitch } from './ButtonLangSwith';
+import { useTranslation } from "react-i18next";
+import { ButtonLangSwitch } from "./ButtonLangSwith";
 
 export const LanguageSwitcher = () => {
   const { i18n } = useTranslation();
 
   const changeLanguage = (lng: string) => {
     i18n.changeLanguage(lng);
-    localStorage.setItem('language', lng);
+    localStorage.setItem("language", lng);
   };
 
   const currentLanguage = i18n.language;
 
   return (
     <div className="fixed top-2 right-2 sm:right-4 sm:top-4 flex gap-2 z-50">
-      <ButtonLangSwitch
-        onChange={(lang) => changeLanguage(lang)}
-        isActive={currentLanguage === 'es'}
-        languageKey="es"
-        ariaLabel="Cambiar a español"
-      >
-        ES
-      </ButtonLangSwitch>
-      <ButtonLangSwitch
-        onChange={(lang) => changeLanguage(lang)}
-        isActive={currentLanguage === 'en'}
-        languageKey="en"
-        ariaLabel="Change to English"
-      >
-        EN
-      </ButtonLangSwitch>
+      {currentLanguage === "es" ? (
+        <ButtonLangSwitch
+          onChange={(lang) => changeLanguage(lang)}
+          languageKey="en"
+          ariaLabel="Change to English"
+        >
+          EN
+        </ButtonLangSwitch>
+      ) : (
+        <ButtonLangSwitch
+          onChange={(lang) => changeLanguage(lang)}
+          languageKey="es"
+          ariaLabel="Cambiar a español"
+        >
+          ES
+        </ButtonLangSwitch>
+      )}
     </div>
   );
 };
-
