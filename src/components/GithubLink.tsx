7143ae12ae5def82432088d520cef42cc6d9639d
vladimirLoza <vloza@getoptimachain.com>
2025-12-28 18:09:57 -0600

    style(Quote, GithubLink): improve responsive

--- src/components/GithubLink.tsx
+++ src/components/GithubLink.tsx
@@ -3,7 +3,7 @@ export const GithubLink = () => {
     <a
       href="https://github.com/vladimir-loza/memento-mori-calendar"
       target="_blank"
-      className="fixed top-4 left-4 z-50 inline-block transition-opacity hover:opacity-80"
+      className="fixed top-2 left-2 sm:left-4 sm:top-4 z-50 inline-block transition-opacity hover:opacity-80"
       aria-label="Ver repositorio en GitHub"
     >
       <img
